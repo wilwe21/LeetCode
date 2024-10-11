15d88936516b0b17d62d83fabf4d71a1aa363ced
wilwe <wilwe036@gmail.com>
2024-10-11 18:48:02 +0200

    1942

---
+++ code/1942.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn smallest_chair(times: Vec<Vec<i32>>, target_friend: i32) -> i32 {
+        let mut char = 0;
+        for i in 0..times.len() {
+            if times[i][0] < times[target_friend as usize][0] {
+                char += 1
+            }
+            if times[i][1] < times[target_friend as usize][0]{
+                char -= 1
+            }
+        }
+        char
+    }
+}
